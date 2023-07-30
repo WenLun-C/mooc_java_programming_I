73f32d3b54c7497d8fe0187fa60853a9d2acd10b
Chan <wenlun.chan@thermofisher.com>
2023-07-30 22:05:21 +0800

    Update part 4

---
+++ Part 4/part04-Part04_05.Whistle/src/main/java/Whistle.java
@@ -0,0 +1,12 @@
+public class Whistle {
+    private String sound;
+
+    public Whistle(String name) {
+        this.sound = name;
+    }
+
+    public void sound() {
+        System.out.println(this.sound);
+    }
+    
+}
